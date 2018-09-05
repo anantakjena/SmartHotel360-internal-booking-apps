d76603564b5dc02808e3a3262fcb77d13678a01b
Mikkel Mork Hegnhoj <mikhegn@microsoft.com>
2018-09-05 20:55:13 +0200

    Adding folder with Mesh deployable containers.

--- Containerizing the original application.md
+++ Containerizing the original application.md
@@ -1,4 +1,11 @@
 # Containerize an existing .NET application
+
 This document will walk you through how to containerize the existing WebForms and WCF services.
 
-[For now this is intentionally left blank]
+1. Run the ./build.ps1 script to build the app and containers
+
+1. Run the ./run.ps1 script to run the containers locally (this includes SQL Express in a container)
+
+1. Browse to http://localhost:8080
+
+1. Run the ./stop.ps1 script to stop all three containers
---
+++ src/Registration-mesh/SmartHotel.Registration/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SmartHotel.Registration
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Set App Insights Instrumentation Key
+            string appInsightsInstrumentationKey = Environment.GetEnvironmentVariable("AppInsightsKey");
+
+            if (!String.IsNullOrEmpty(appInsightsInstrumentationKey))
+            {
+                TelemetryConfiguration.Active.InstrumentationKey = appInsightsInstrumentationKey;
+            }
+
+            Trace.TraceInformation($"AI Key: {appInsightsInstrumentationKey}");
+        }
+    }
+}
